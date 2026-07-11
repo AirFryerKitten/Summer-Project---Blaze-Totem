0aa0312f2e5df474341e75dd1987ae45913d6b02
AirFryer Kitten <airfryerkitten@gmail.com>
2026-07-10 21:40:09 -0400

    Started Project

---
+++ main.py
@@ -0,0 +1,15 @@
+#Jonas York 
+#Summer Project 
+#Making a game 
+
+
+#This is a summer Project to help me make sure I dont forget everything Ive learned 
+#The game is called Blaze Totem and is based off Intelligent System's Fire Emblem Series 
+#There is only going to be one chapter/map at the moment 
+#Also there is no tutorial on how to play. Just like get a copy of Fire Emblem 7 or Fire Emblem 8 and do the tutorial there
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
