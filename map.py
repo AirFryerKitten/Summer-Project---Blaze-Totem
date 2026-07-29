0f42cdb0ca483e6d4658a09e4e324ce07085d56a
Jonas <airfryerkitten@gmail.com>
2026-07-28 22:04:41 -0400

    started map

---
+++ map.py
@@ -0,0 +1,14 @@
+#Map this will have info about the whole map and the tiles within it
+
+class Map:
+
+
+    class Tile:
+        def __init__(self):
+            self.occupied = False
+            
+
+    def __init__(self):
+        self.map = []
+
+    
